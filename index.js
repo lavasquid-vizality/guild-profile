cc4ce03ffc80862f9f79b361c73aed84e0b8d1d6
LavaSquids <79622877+LavaSquids@users.noreply.github.com>
2022-04-25 08:09:02 +0100

    Change `patchContextMenuLazy` to `getContextMenuLazy`
    Change to a system which doesn't automatically patch, but return module as a promise

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Plugin } from '@vizality/entities';
+import { patch } from '@vizality/patcher';
 import { getModule } from '@vizality/webpack';
 
 import GuildProfileModal from './components/GuildProfileModal';
 
 import MemberCountStore from './stores/memberCount';
 import GFCheck from './modules/GFCheck';
-import patchContextMenuLazy from './modules/patchContextMenuLazy';
+import getContextMenuLazy from './modules/getContextMenuLazy';
 
 const { MenuGroup, MenuItem } = getModule(m => m.MenuItem);
 
@@ -22,13 +23,13 @@ export default class GuildProfile extends Plugin {
   }
 
   patch () {
-    patchContextMenuLazy(getModule.bind(this, m => m.default?.displayName === 'GuildContextMenu'), 'default', (args, res) => {
+    getContextMenuLazy(getModule.bind(this, m => m.default?.displayName === 'GuildContextMenu')).then(module => patch(module, 'default', (args, res) => {
       const { guild } = args[0];
 
       res.props.children.unshift(<MenuGroup><MenuItem action={() => openModalLazy(() => ModalArgs => <GuildProfileModal {...ModalArgs} guild={guild} />)} id={'guild-profile'} label={'Server Profile'} /></MenuGroup>);
 
       return res;
-    });
+    }));
   }
 
   stop () {
